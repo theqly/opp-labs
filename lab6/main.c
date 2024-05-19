6b2fa6bc438c976523d8c8d919dfcbdaf8780d53
theqly <theqlgames@gmail.com>
2024-05-19 15:46:49 +0700

    lab6 refactor

--- lab6/main.c
+++ lab6/main.c
@@ -9,7 +9,7 @@
 #define SENDING_TASKS 555
 
 #define L 2048
-#define TASKS 2048
+#define TASKS 1024
 
 struct data{
     int size, rank;
@@ -92,41 +92,18 @@ void* worker_func(void* void_data){
         MPI_Allreduce(&local_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
 
         MPI_Barrier(MPI_COMM_WORLD);
-
-        disbalance = max_time - min_time;
-        dolya_disbalace = (disbalance/max_time) * 100;
-
         printf("rank = %d; iteration = %d; time = %lf; tasks done = %d; result = %lf\n", data->rank, i, local_time, data->tasks_done, data->result);
-        if(data->rank == 0) printf("disbalace: %lf and dolya: %lf at iteration %d\n", disbalance, dolya_disbalace, i);
 
+        if(data->rank==0) {
+            disbalance = max_time - min_time;
+            dolya_disbalace = (disbalance/max_time) * 100;
+            printf("disbalace: %lf and dolya: %lf at iteration %d\n", disbalance, dolya_disbalace, i);
+        }
     }
 
     int finish = 999;
     MPI_Send(&finish, 1, MPI_INT, data->rank, HELP_START, MPI_COMM_WORLD);
     free(data->tasks);
-    pthread_exit(NULL);
-}
-
-void* receiver_func(void* void_data){
-    struct data* data = (struct data*) void_data;
-    int message; // процесс, который закончил работу, или флаг завершения работы
-    MPI_Barrier(MPI_COMM_WORLD);
-    while(1){
-        MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE, HELP_START, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        if(message == 999) break;
-        pthread_mutex_lock(&data->mutex);
-        int tasks_remained = data->tasks_to_do - data->tasks_done;
-        int tasks_to_send = tasks_remained / (data->size * 2);
-        if(tasks_remained >= 1 && tasks_to_send >= 1){
-            data->tasks_to_do -= tasks_to_send;
-            MPI_Send(&tasks_to_send, 1, MPI_INT, message, HELP_COUNT, MPI_COMM_WORLD);
-            MPI_Send(&data->tasks[data->tasks_to_do - tasks_to_send], tasks_to_send, MPI_INT, message, SENDING_TASKS, MPI_COMM_WORLD);
-        } else {
-            tasks_to_send = 0;
-            MPI_Send(&tasks_to_send, 1, MPI_INT, message, HELP_COUNT, MPI_COMM_WORLD);
-        }
-        pthread_mutex_unlock(&data->mutex);
-    }
     pthread_exit(NULL);
 }
 
@@ -149,19 +126,8 @@ int main(int argc, char** argv){
         MPI_Finalize();
         return 1;
     }
-    if (pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE) != 0){
-        fprintf(stderr, "cant set attr as PTHREAD_CREATE_JOINABLE\n");
-        MPI_Finalize();
-        return 1;
-    }
 
-    pthread_t worker, receiver;
-
-    if(pthread_create(&receiver, &attr, receiver_func, data) != 0){
-        fprintf(stderr, "error in creating receiver\n");
-        MPI_Finalize();
-        return 1;
-    }
+    pthread_t worker;
 
     if(pthread_create(&worker, &attr, worker_func, data) != 0){
         fprintf(stderr, "error in creating worker\n");
@@ -170,11 +136,26 @@ int main(int argc, char** argv){
     }
 
     double start = MPI_Wtime();
+    int message; // процесс, который закончил работу, или флаг завершения работы
+    MPI_Barrier(MPI_COMM_WORLD);
+    while(1){
+        MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE, HELP_START, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if(message == 999) break;
+        pthread_mutex_lock(&data->mutex);
+        int tasks_remained = data->tasks_to_do - data->tasks_done;
+        pthread_mutex_unlock(&data->mutex);
+        int tasks_to_send = tasks_remained / (data->size * 2);
+        if(tasks_remained >= 1 && tasks_to_send >= 1){
+            pthread_mutex_lock(&data->mutex);
+            data->tasks_to_do -= tasks_to_send;
+            pthread_mutex_unlock(&data->mutex);
+            MPI_Send(&tasks_to_send, 1, MPI_INT, message, HELP_COUNT, MPI_COMM_WORLD);
+            MPI_Send(&data->tasks[data->tasks_to_do - tasks_to_send], tasks_to_send, MPI_INT, message, SENDING_TASKS, MPI_COMM_WORLD);
+        } else {
+            tasks_to_send = 0;
+            MPI_Send(&tasks_to_send, 1, MPI_INT, message, HELP_COUNT, MPI_COMM_WORLD);
+        }
 
-    if(pthread_join(receiver, NULL) != 0){
-        fprintf(stderr, "error in joining receiver\n");
-        MPI_Finalize();
-        return 1;
     }
 
     if(pthread_join(worker, NULL) != 0){
